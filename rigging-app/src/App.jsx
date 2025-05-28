6124ddef02f730afee9c51d9c28e8f7d644b15b9
bagnukcompany <bagnukcompany@gmail.com>
2025-05-28 15:32:06 -0400

    added react
    added react

---
+++ rigging-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="p-6 text-xl font-bold text-blue-800">
+            Rigging Configurator Starter
+        </div>
+    );
+}
+
+export default App;
